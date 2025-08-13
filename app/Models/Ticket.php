bfdc42f5ef61e5286b126c59dec8ca35381cadc0
Adhit <adhityafbrnsyhptra@gmail.com>
2025-08-13 15:02:15 +0700

    update

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+        'movie_id', 'studio_id', 'city_id', 'cinema_id', 'date', 'time', 'price'
+    ];
+
+    public function movie()
+    {
+        return $this->belongsTo(Movie::class);
+    }
+
+    public function studio()
+    {
+        return $this->belongsTo(Studio::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function cinema()
+    {
+        return $this->belongsTo(Cinema::class);
+    }
+
+    public function seats()
+    {
+        return $this->hasMany(Seat::class);
+    }
 }
